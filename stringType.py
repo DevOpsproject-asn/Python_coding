6dce209e4c24a1331116072ad631d91e10eb5d18
AvinnaSN <141218244+DevOpsproject-asn@users.noreply.github.com>
2025-03-29 17:29:43 +0530

    Create stringType.py

---
+++ stringType.py
@@ -0,0 +1,28 @@
+s="  You are the best "
+print(s)
+print(type(s))
+
+s1="""You are the creator of your destiny"""
+print(s1)
+
+print(s[0])
+
+print(s[2])
+
+print(s*3)
+print(len(s1))
+print(len(s))
+#slicing
+print(s[0:5])
+print(s[0:])
+print(s[:8])
+
+print(s[-3:-1])
+print(s[0:9:2])
+print(s[15::-1])
+print(s[::-1])
+print(s.strip())
+print(s.lstrip())
+print(s.rstrip())
+print(s.find("the",0,20))
+print(s.count("a"))
