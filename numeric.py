bf483dd961ac5051d6b1b4edd13a091471c78be9
AvinnaSN <141218244+DevOpsproject-asn@users.noreply.github.com>
2025-03-29 17:28:32 +0530

    Create numeric.py

---
+++ numeric.py
@@ -0,0 +1,45 @@
+userID = 123
+USer123id123 =123
+user_id = 234
+_user_id_ = 256
+_userID ( private )
+__userID ( double private vary vary private)
+__init__ 
+We can't start with numberic number
+
+a=13
+b=100
+c=66
+print(a,b,c)
+print(type(a))
+x=33.56
+y=-25.89
+z=206.0
+print(x,y,z)
+print(type(y))
+d=3+5j
+print(d)
+print(type(d))
+
+e=0B1010
+print(e)
+print(type(e))
+
+f=0XFF
+print(f)
+print(type(f))
+
+g=True
+print(g)
+print(type(g))
+print(9>8)
+
+h=int(x)
+print(h)
+
+i=float("22.5")
+print(i)
+print(bin(10))
+print(float(15))
+print(hex(10))
+print(oct(10))
